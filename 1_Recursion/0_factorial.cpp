fd46048c15d81e46ee01f840ffd520a68e0c36df
Naumaan Saifi <naumaansaifi7@gmail.com>
2025-10-21 14:24:48 +0530

    Recursion_1

---
+++ 1_Recursion/0_factorial.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int factorial(int n){
+    if (n==0 || n==1) return 1;
+    else 
+        return n * factorial(n-1);  
+}
+int main(){
+    cout<<factorial(5);
+}
