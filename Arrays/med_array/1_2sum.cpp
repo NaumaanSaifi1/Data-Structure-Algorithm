afae384167d40de1ee190de26616a5064003e01a
Naumaan Saifi <naumaansaifi7@gmail.com>
2025-11-02 15:31:09 +0530

    ..

--- Arrays/med_array/1_2sum.cpp
+++ Arrays/med_array/1_2sum.cpp
@@ -1,23 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hash;  // value -> index mapping
-        // vector<int> res;
-
+        
         for (int i = 0; i < nums.size(); ++i) {
             int comp = target - nums[i]; // complement
 
             if (hash.find(comp) != hash.end()) {
-                // // found the pair
-                // res.push_back(hash[comp]);
-                // res.push_back(i);
-                return {hash[comp],i};  // we can return immediately after finding
+                return {hash[comp], i};  // we can return immediately after finding
             }
 
             // store current value with its index
             hash[nums[i]] = i;
         }
 
-        return {-1,-1};  // empty if not found
+        return {-1,-1};  // if no solution found
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    
+    vector<int> result = sol.twoSum(nums, target);
+    
+    if (result[0] != -1) {
+        cout << "Indices found: [" << result[0] << ", " << result[1] << "]" << endl;
+        cout << "Numbers: " << nums[result[0]] << " + " << nums[result[1]] << " = " << target << endl;
+    } else {
+        cout << "No solution found!" << endl;
+    }
+    
+    return 0;
+}
